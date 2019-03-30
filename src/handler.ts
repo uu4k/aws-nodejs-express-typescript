6f2bf6faaed29df7e5194a8412ddfe5d4bc979dd
uu4k <t.shimizu1115@gmail.com>
2019-03-31 07:55:47 +0900

    fix handler.

--- src/handler.ts
+++ src/handler.ts
@@ -1,8 +1,10 @@
+import { Request, Response } from 'express'
+
 const serverless = require('serverless-http')
 const express = require('express')
 const app = express()
 
-app.get('/', function(_req, res) {
+app.get('/', (_req: Request, res: Response) => {
   res.json({ message: 'Hello World!' })
 })
 
