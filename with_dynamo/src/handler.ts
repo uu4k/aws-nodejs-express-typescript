a2b5b5ef9fd33a5be614707f6ac40f6197f2ce56
uu4k <t.shimizu1115@gmail.com>
2019-03-31 11:35:29 +0900

    add dynamo template.

--- with_dynamo/src/handler.ts
+++ with_dynamo/src/handler.ts
@@ -1,13 +1,44 @@
-import { Request, Response } from 'express'
-const serverless = require('serverless-http')
-const express = require('express')
-const app = express()
-const router = require('express-promise-router')()
+import * as express from 'express'
+import serverless from 'serverless-http'
+import Router from 'express-promise-router'
 
-router.get('/', (_req: Request, res: Response) => {
+const app = express()
+const router = Router()
+
+import * as aws from 'aws-sdk'
+
+const getDynamoClient = (ip: string): aws.DynamoDB.DocumentClient => {
+  let dynamodb = null
+  if (ip === '127.0.0.1' || ip === 'localhost') {
+    dynamodb = new aws.DynamoDB.DocumentClient({
+      region: 'localhost',
+      endpoint: 'http://localhost:8000'
+    })
+  } else {
+    dynamodb = new aws.DynamoDB.DocumentClient()
+  }
+  return dynamodb
+}
+
+router.get('/', (_req: express.Request, res: express.Response) => {
   res.json({ message: 'Hello World!' })
+})
+
+router.get('/:id', async (req: express.Request, res: express.Response) => {
+  const dynamodb = getDynamoClient(req.ip)
+  const params: aws.DynamoDB.DocumentClient.GetItemInput = {
+    TableName: 'hello',
+    Key: {
+      id: req.params.id
+    }
+  }
+  const result: AWS.DynamoDB.GetItemOutput = await dynamodb
+    .get(params)
+    .promise()
+
+  res.json({ hello: result.Item })
 })
 
 app.use('/', router)
 
-module.exports.main = serverless(app)
+export const main = serverless(app)
